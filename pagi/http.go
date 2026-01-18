64234ca15781503cf93763d93082367b0ba7df0c
trpdjke <yehorushakov@gmail.com>
2026-01-18 21:31:32 +0200

    builf page

--- pagi/http.go
+++ pagi/http.go
@@ -82,3 +82,76 @@ func GetPagination(r *http.Request) (limit uint, offset uint) {
 
 	return
 }
+
+type Links struct {
+	Self  string  `json:"self"`
+	First *string `json:"first,omitempty"`
+	Last  *string `json:"last,omitempty"`
+	Prev  *string `json:"prev,omitempty"`
+	Next  *string `json:"next,omitempty"`
+}
+
+func BuildPageLinks(r *http.Request, page, size, total uint) Links {
+	if page == 0 {
+		page = 1
+	}
+	if size == 0 {
+		size = 20
+	}
+
+	lastPage := uint(1)
+	if total > 0 {
+		lastPage = (total + size - 1) / size
+		if lastPage == 0 {
+			lastPage = 1
+		}
+	}
+
+	self := buildURLWithPage(r, page, size)
+
+	var first *string
+	if page != 1 {
+		v := buildURLWithPage(r, 1, size)
+		first = &v
+	}
+
+	var last *string
+	if page != lastPage {
+		v := buildURLWithPage(r, lastPage, size)
+		last = &v
+	}
+
+	var prev *string
+	if page > 1 {
+		v := buildURLWithPage(r, page-1, size)
+		prev = &v
+	}
+
+	var next *string
+	if page < lastPage {
+		v := buildURLWithPage(r, page+1, size)
+		next = &v
+	}
+
+	return Links{
+		Self:  self,
+		First: first,
+		Last:  last,
+		Prev:  prev,
+		Next:  next,
+	}
+}
+
+func buildURLWithPage(r *http.Request, page, size uint) string {
+	u := *r.URL
+	q := u.Query()
+
+	q.Set("page", strconv.FormatUint(uint64(page), 10))
+	q.Set("size", strconv.FormatUint(uint64(size), 10))
+
+	q.Del("page[number]")
+	q.Del("page[size]")
+
+	u.RawQuery = q.Encode()
+	return u.String()
+}
